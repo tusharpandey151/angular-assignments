ef25aa2c3ccab232320a9d45f8d83907d7b61c36
Tushar Pandey <tusharpandey151@gmail.com>
2022-04-15 17:42:51 +0530

    updated services start

---
+++ services-start/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AccountsService } from './accounts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
+})
+export class AppComponent {
+  
+  accounts: {name:string, status:string}[] = [];
+
+  constructor(private accountsService:AccountsService) {
+
+  }
+
+  ngOnInit () {
+    this.accounts = this.accountsService.accounts;
+  }
+}
